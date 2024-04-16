c2925ad92dd452b766f566642a8a2ce73d39f43d
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-16 21:11:45 +0330

    expenses

---
+++ expenses/expenses.go
@@ -0,0 +1,67 @@
+package expenses
+
+import "errors"
+
+// Record represents an expense record.
+type Record struct {
+	Day      int
+	Amount   float64
+	Category string
+}
+
+// DaysPeriod represents a period of days for expenses.
+type DaysPeriod struct {
+	From int
+	To   int
+}
+
+// Filter returns the records for which the predicate function returns true.
+func Filter(in []Record, predicate func(Record) bool) []Record {
+	out := []Record{}
+	for _, value := range in {
+		if predicate(value) {
+			out = append(out, value)
+		}
+	}
+	return out
+}
+
+// ByDaysPeriod returns predicate function that returns true when
+// the day of the record is inside the period of day and false otherwise.
+func ByDaysPeriod(p DaysPeriod) func(Record) bool {
+	return func(r Record) bool {
+		return p.From <= r.Day && r.Day <= p.To
+	}
+}
+
+// ByCategory returns predicate function that returns true when
+// the category of the record is the same as the provided category
+// and false otherwise.
+func ByCategory(c string) func(Record) bool {
+	return func(r Record) bool {
+		return c == r.Category
+	}
+}
+
+// TotalByPeriod returns total amount of expenses for records
+// inside the period p.
+func TotalByPeriod(in []Record, p DaysPeriod) float64 {
+	res := 0.0
+	filteredRecords := Filter(in, ByDaysPeriod(p))
+	for _, value := range filteredRecords {
+		res += value.Amount
+	}
+	return res
+}
+
+// CategoryExpenses returns total amount of expenses for records
+// in category c that are also inside the period p.
+// An error must be returned only if there are no records in the list that belong
+// to the given category, regardless of period of time.
+func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
+	a := Filter(in, ByCategory(c))
+	if len(a) == 0 {
+		return 0.0, errors.New("unknown category entertainment")
+	}
+	return TotalByPeriod(a, p), nil
+}
