b15d4907a61ba59dc70d402ca38ed675891a240e
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-17 09:14:15 +0330

    collatz-conjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -6,9 +6,9 @@ func CollatzConjecture(n int) (i int, err error) {
 	if n < 1 {
 		return 0, errors.New("input number should be more than 0")
 	}
-	for i := 0; n != 1; i++ {
+	for i = 0; n != 1; i++ {
 		if n%2 == 0 {
-			n = n / 2
+			n /= 2
 		} else {
 			n = 3*n + 1
 		}
