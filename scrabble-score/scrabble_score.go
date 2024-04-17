e7975e07927c5b41e8a2b08301073e14b706d2ed
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-17 13:09:18 +0330

    scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,38 +1,25 @@
 package scrabble
 
-import "strings"
+import "unicode"
 
 func Score(word string) (sum int) {
-	mapLetterValue := map[rune]int{
-		'A': 1,
-		'E': 1,
-		'I': 1,
-		'O': 1,
-		'U': 1,
-		'L': 1,
-		'N': 1,
-		'R': 1,
-		'S': 1,
-		'T': 1,
-		'D': 2,
-		'G': 2,
-		'B': 3,
-		'C': 3,
-		'M': 3,
-		'P': 3,
-		'F': 4,
-		'H': 4,
-		'V': 4,
-		'W': 4,
-		'Y': 4,
-		'K': 5,
-		'J': 8,
-		'X': 8,
-		'Q': 10,
-		'Z': 10,
+	for _, letter := range word {
+		switch unicode.ToLower(letter) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			sum += 1
+		case 'd', 'g':
+			sum += 2
+		case 'b', 'c', 'm', 'p':
+			sum += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			sum += 4
+		case 'k':
+			sum += 5
+		case 'j', 'x':
+			sum += 8
+		case 'q', 'z':
+			sum += 10
+		}
 	}
-	for _, value := range strings.ToUpper(word) {
-		sum += mapLetterValue[value]
-	}
-	return
+	return sum
 }
