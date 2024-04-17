8562addb4621bfc685e0da49f982dd31167071cf
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-17 12:53:10 +0330

    raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,12 +6,10 @@ import (
 
 func Convert(number int) string {
 	result := ""
-	conditions := [...]map[int]string{{3: "Pling"}, {5: "Plang"}, {7: "Plong"}}
-	for _, value := range conditions {
-		for key, val := range value {
-			if number%key == 0 {
-				result += val
-			}
+	conditions := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
+	for key, value := range conditions {
+		if number%key == 0 {
+			result += value
 		}
 	}
 	if result == "" {
