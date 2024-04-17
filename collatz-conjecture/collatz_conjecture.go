c13c85d287945284c6c3f40e17f9cf6dfc88dba8
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-17 09:06:50 +0330

    collatz-conjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -2,20 +2,16 @@ package collatzconjecture
 
 import "errors"
 
-func CollatzConjecture(n int) (int, error) {
-	count := 0
+func CollatzConjecture(n int) (i int, err error) {
 	if n < 1 {
 		return 0, errors.New("input number should be more than 0")
 	}
-	count++
-	if n == 1 {
-		return 0, nil
-	} else if n%2 == 0 {
-		a, b := CollatzConjecture(n / 2)
-		return a + count, b
-	} else if n%2 == 1 {
-		a, b := CollatzConjecture(3*n + 1)
-		return a + count, b
+	for i := 0; n != 1; i++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
 	}
-	return count, nil
+	return
 }
