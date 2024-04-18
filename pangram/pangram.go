1e9be459ec700bedad560b660d1047b9188f84be
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 07:48:43 +0330

    pangram

---
+++ pangram/pangram.go
@@ -0,0 +1,19 @@
+package pangram
+
+import (
+	"strings"
+)
+
+func IsPangram(input string) bool {
+	result := uint32(0)
+	for _, value := range strings.ToLower(input) {
+		if value == ' ' || value == '_' || value == '.' || value == '"' || ('0' <= value && value <= '9') {
+			continue
+		}
+		if value < 'a' || 'z' < value {
+			return false
+		}
+		result |= 1 << (value - 'a')
+	}
+	return result == 0x3ffffff
+}
