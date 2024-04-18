8b889e8c7954fe321c75ff6873b36bf476e52f9b
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 08:01:15 +0330

    pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,20 +1,16 @@
 package pangram
 
-import (
-	"strings"
-)
-
 func IsPangram(input string) bool {
 	result := uint32(0)
-	for _, value := range strings.ToLower(input) {
+	for _, value := range input {
 		switch value := value; {
 		case value == ' ', value == '_', value == '.', value == '"', '0' <= value && value <= '9':
 			continue
-		case value < 'a' || 'z' < value:
-			return false
-		default:
+		case 'a' <= value && value <= 'z':
 			result |= 1 << (value - 'a')
+		case 'A' <= value && value <= 'Z':
+			result |= 1 << (value - 'A')
 		}
 	}
-	return result == 0x3ffffff
+	return result == 0b11111111111111111111111111
 }
