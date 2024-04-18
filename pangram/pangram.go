3666ab90ba7c443487e2e932d9accb7746bb9b30
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 08:05:00 +0330

    pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,11 +1,22 @@
 package pangram
 
 func IsPangram(input string) bool {
+	// const alphamask int = 0b11111111111111111111111111
+	// result := 0
+	// length := len(input)
+	// for i := 0; i < length; i++ {
+	// 	value := input[i]
+	// 	if value > 96 && value < 123 {
+	// 		result |= 1 << (value - 97)
+	// 	} else if value > 64 && value < 91 {
+	// 		result |= 1 << (value - 65)
+	// 	}
+	// }
+	// return alphamask&result == alphamask
+
 	result := uint32(0)
 	for _, value := range input {
 		switch value := value; {
-		case value == ' ', value == '_', value == '.', value == '"', '0' <= value && value <= '9':
-			continue
 		case 'a' <= value && value <= 'z':
 			result |= 1 << (value - 'a')
 		case 'A' <= value && value <= 'Z':
