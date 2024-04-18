d1ede1456c6d0147f1b3d3a5bf5961b56b7423f8
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 08:12:15 +0330

    pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -14,14 +14,14 @@ func IsPangram(input string) bool {
 	// }
 	// return alphamask&result == alphamask
 
-	result := uint32(0)
+	const alphamask int = 0b11111111111111111111111111
+	result := 0
 	for _, value := range input {
-		switch value := value; {
-		case 'a' <= value && value <= 'z':
-			result |= 1 << (value - 'a')
-		case 'A' <= value && value <= 'Z':
-			result |= 1 << (value - 'A')
+		if value > 96 && value < 123 {
+			result |= 1 << (value - 97)
+		} else if value > 64 && value < 91 {
+			result |= 1 << (value - 65)
 		}
 	}
-	return result == 0b11111111111111111111111111
+	return alphamask&result == alphamask
 }
