d017c9f68a786c8c40ac8a31947ac6f742c32c90
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 20:29:04 +0330

    roman-numerals

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -2,35 +2,30 @@ package romannumerals
 
 import (
 	"errors"
-	"math"
 )
 
 func ToRomanNumeral(input int) (string, error) {
 	if input < 1 || 3999 < input {
 		return "", errors.New("error")
 	}
-	romans := map[int]string{
-		1000: "M",
-		900:  "CM",
-		500:  "D",
-		400:  "CD",
-		100:  "C",
-		90:   "XC",
-		50:   "L",
-		40:   "XL",
-		10:   "X",
-		9:    "IX",
-		5:    "V",
-		4:    "IV",
-		1:    "I",
+	var romanNum = map[int]string{
+		1000: "M", 2000: "MM", 3000: "MMM",
+		100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM",
+		10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
+		1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX",
 	}
-	res := ""
-	for index, value := range romans {
-		max := int(math.Floor(float64(input / index)))
-		for i := 0; i < max; i++ {
-			res += value
-		}
-		input -= index * max
-	}
-	return res, nil
+	roman := ""
+	// thousands
+	k := input / 1000
+	roman += romanNum[k*1000]
+	// hundreds
+	c := (input / 100) % 10
+	roman += romanNum[c*100]
+	// tens
+	d := (input / 10) % 10
+	roman += romanNum[d*10]
+	// units
+	u := (input / 1) % 10
+	roman += romanNum[u*1]
+	return roman, nil
 }
