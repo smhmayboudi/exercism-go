e136b1094ae9ee028f74cc891a594d41575f59b0
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 19:32:58 +0330

    roman-numerals

---
+++ roman-numerals/roman_numerals.go
@@ -0,0 +1,31 @@
+package romannumerals
+
+import "math"
+
+func ToRomanNumeral(input int) (string, error) {
+	panic("Please implement the ToRomanNumeral function")
+	romans := map[int]string{
+		1000: "M",
+		900:  "CM",
+		500:  "D",
+		400:  "CD",
+		100:  "C",
+		90:   "XC",
+		50:   "L",
+		40:   "XL",
+		10:   "X",
+		9:    "IX",
+		5:    "V",
+		4:    "IV",
+		1:    "I",
+	}
+	res := ""
+	for index, value := range romans {
+		max := int(math.Floor(float64(input / index)))
+		for i := 0; i < max; i++ {
+			res += value
+		}
+		input -= index * max
+	}
+	return res, nil
+}
