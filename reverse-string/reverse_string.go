62eeec0672d98e64c8a4e0659153bf0ecb511fdd
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 08:48:33 +0330

    reverse-string

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -3,15 +3,8 @@ package reverse
 func Reverse(input string) string {
 	in := []rune(input)
 	le := len(in)
-	// for i, j := 0, le-1; i < le; i, j = i+1, j-1 {
-	// 	in[i], in[j] = in[j], in[i]
-	// }
-	i := 0
-	j := le - 1
-	for i < j {
-		in[i], in[j] = in[j], in[i]
-		i++
-		j--
+	for i := 0; i < le/2; i++ {
+		in[i], in[le-1-i] = in[le-1-i], in[i]
 	}
 	return string(in)
 }
