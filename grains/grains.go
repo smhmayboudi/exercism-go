c03c4ba37d2afe4e7cc5097a6f4e8f6bf062036e
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-18 18:47:24 +0330

    grains

--- grains/grains.go
+++ grains/grains.go
@@ -2,16 +2,15 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 func Square(number int) (uint64, error) {
 	if number < 1 || 64 < number {
 		return 0, errors.New("error")
 	}
-	return uint64(math.Pow(2, float64(number-1))), nil
+	return 1 << (number - 1), nil
 }
 
 func Total() uint64 {
-	return 0b1111111111111111111111111111111111111111111111111111111111111111
+	return 1<<64 - 1
 }
