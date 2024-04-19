50ccc27e710bf69b8fc04b61a0f00cf0a1ea6987
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-19 08:56:34 +0330

    resistor-color-duo

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,27 +1,33 @@
 package resistorcolorduo
 
+func Color(color string) int {
+	switch color {
+	case "black":
+		return 0
+	case "brown":
+		return 1
+	case "red":
+		return 2
+	case "orange":
+		return 3
+	case "yellow":
+		return 4
+	case "green":
+		return 5
+	case "blue":
+		return 6
+	case "violet":
+		return 7
+	case "grey":
+		return 8
+	case "white":
+		return 9
+	default:
+		return -1
+	}
+}
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	sum := 0
-	bandColors := map[string]int{
-		"black":  0,
-		"brown":  1,
-		"red":    2,
-		"orange": 3,
-		"yellow": 4,
-		"green":  5,
-		"blue":   6,
-		"violet": 7,
-		"grey":   8,
-		"white":  9,
-	}
-	for index, value := range bandColors {
-		if colors[0] == index {
-			sum += 10 * value
-		}
-		if colors[1] == index {
-			sum += value
-		}
-	}
-	return sum
+	return Color(colors[0])*10 + Color(colors[1])
 }
