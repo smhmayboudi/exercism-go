052e0f8a1002831f8ccd09abde40cdf87a492788
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-20 10:06:29 +0330

    armstrong-numbers

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,17 +1,19 @@
 package armstrong
 
-import (
-	"math"
-	"strconv"
-)
-
+// IsNumber determines whether a number is an Armstrong number.
 func IsNumber(number int) bool {
-	numString := []rune(strconv.Itoa(number))
-	numDigits := len(numString)
-	sum := 0
-	for _, value := range numString {
-		num, _ := strconv.Atoi(string(value))
-		sum += int(math.Pow(float64(num), float64(numDigits)))
+	exponent := 0
+	for n := number; n > 0; n /= 10 {
+		exponent++
 	}
-	return sum == number
+	sum := 0
+	for n := number; n > 0; n /= 10 {
+		digit := n % 10
+		power := 1
+		for j := 0; j < exponent; j++ {
+			power *= digit
+		}
+		sum += power
+	}
+	return number == sum
 }
