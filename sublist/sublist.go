edb1af62ea5609385f48095ee25e3aae1553306a
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-20 15:29:54 +0330

    sublist

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -1,30 +1,38 @@
 package sublist
 
-import "reflect"
-
 // Relation type is defined in relations.go file.
 
 func Sublist(l1, l2 []int) Relation {
-	if reflect.DeepEqual(l1, l2) {
-		return RelationEqual
+	len1, len2 := len(l1), len(l2)
+	switch {
+	case len1 < len2:
+		return testSubList(l1, l2)
+	case len1 > len2:
+		return testSuperlist(l1, l2)
+	default:
+		return testEqual(l1, l2)
 	}
-	if contains(l2, l1) {
-		return RelationSublist
+}
+func testEqual(l1, l2 []int) Relation {
+	for i := 0; i < len(l1); i++ {
+		if l1[i] != l2[i] {
+			return RelationUnequal
+		}
 	}
-	if contains(l1, l2) {
-		return RelationSuperlist
+	return RelationEqual
+}
+func testSubList(l1, l2 []int) Relation {
+	len1, len2 := len(l1), len(l2)
+	for i := 0; i+len1 <= len2; i++ {
+		if testEqual(l1, l2[i:i+len1]) == RelationEqual {
+			return RelationSublist
+		}
 	}
 	return RelationUnequal
 }
-
-func contains(s1, s2 []int) bool {
-	i, j := 0, 0
-	for ; i < len(s1) && j < len(s2); i++ {
-		if s1[i] == s2[j] {
-			j++
-		} else {
-			i, j = i-j, 0
-		}
+func testSuperlist(l1, l2 []int) Relation {
+	if testSubList(l2, l1) == RelationSublist {
+		return RelationSuperlist
 	}
-	return j == len(s2)
+	return RelationUnequal
 }
