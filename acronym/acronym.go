bc3962da616a32f2e3542303b2d16c19d81819ce
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-20 16:29:26 +0330

    acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,6 +1,9 @@
 package acronym
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func Abbreviate(s string) string {
 	str := []rune(s)
@@ -8,9 +11,9 @@ func Abbreviate(s string) string {
 	sb.Grow(len(str) / 2)
 	for i := 0; i < len(str); i++ {
 		if i == 0 || (1 < i && (str[i-1] == ' ' || str[i-1] == '-' || str[i-1] == '_')) {
-			if 'a' <= str[i] && str[i] <= 'z' {
+			if unicode.IsLower(str[i]) {
 				sb.WriteRune(str[i] - 'a' + 'A')
-			} else if 'A' <= str[i] && str[i] <= 'Z' {
+			} else if unicode.IsUpper(str[i]) {
 				sb.WriteRune(str[i])
 			}
 		}
