aa6a09f2168b63008865b8ccf9e228437f3f8c11
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-21 07:11:55 +0330

    twelve-days

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -8,10 +8,6 @@ func Verse(i int) string {
 	if i < 1 || 12 < i {
 		return ""
 	}
-	return strings.Split(Song(), "\n")[i-1]
-}
-
-func Song() string {
 	a := []string{
 		"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
 		"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
@@ -25,6 +21,14 @@ func Song() string {
 		"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 		"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 		"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	}
+	return a[i-1]
+}
+
+func Song() string {
+	a := []string{}
+	for i := 0; i < 12; i++ {
+		a = append(a, Verse(i+1))
 	}
 	return strings.Join(a, "\n")
 }
