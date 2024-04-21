f3172cf8cf8c2dcb99b5c5b1399afb1aca9adc38
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-21 17:39:28 +0330

    meetup

---
+++ meetup/meetup.go
@@ -0,0 +1,23 @@
+package meetup
+
+import "time"
+
+// Define the WeekSchedule type here.
+type WeekSchedule int
+
+const (
+	First  WeekSchedule = 1
+	Second              = 8
+	Third               = 15
+	Fourth              = 22
+	Teenth              = 13
+	Last                = -6
+)
+
+func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
+	if wSched == Last {
+		month++
+	}
+	t := time.Date(year, month, int(wSched), 0, 0, 0, 0, time.UTC)
+	return t.Day() + int(wDay-t.Weekday()+7)%7
+}
