5807787aa9cf7beb70ae0edcc5ded255b52b7b36
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-22 22:07:00 +0330

    robot-name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,34 +1,49 @@
 package robotname
 
 import (
+	"fmt"
 	"math/rand"
-	"strings"
 	"time"
 )
 
 // Define the Robot type here.
 type Robot string
 
-func (r *Robot) Name() (string, error) {
-	if *r == "" {
-		var sb strings.Builder
-		sb.Grow(5)
-		var run rune
-		rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-		for i := 0; i < 2; i++ {
-			run = 'A' + rune(rng.Intn(26))
-			sb.WriteRune(run)
+func generateRobotNames() []string {
+	pos := 0
+	const maxRobotNames = 26 * 26 * 10 * 10 * 10
+	names := make([]string, maxRobotNames)
+
+	for i := 'A'; i <= 'Z'; i++ {
+		for j := 'A'; j <= 'Z'; j++ {
+			for k := 0; k < 1000; k++ {
+				names[pos] = fmt.Sprintf("%s%s%03d", string(i), string(j), k)
+				pos++
+			}
 		}
-		for i := 0; i < 3; i++ {
-			run = '0' + rune(rng.Intn(9))
-			sb.WriteRune(run)
-		}
-		*r = Robot(sb.String())
 	}
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	rng.Shuffle(len(names), func(i, j int) { names[i], names[j] = names[j], names[i] })
+	return names
+}
+
+var (
+	idx      = 0
+	namePool = generateRobotNames()
+)
+
+func (r *Robot) Name() (string, error) {
+	if *r != "" {
+		return string(*r), nil
+	}
+
+	*r = Robot(namePool[idx])
+	idx++
+
 	return string(*r), nil
 }
 
 func (r *Robot) Reset() {
 	*r = ""
-	r.Name()
 }
