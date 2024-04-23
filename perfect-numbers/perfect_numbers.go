581ebed2d1028ea5b1a3149db1c68e36ac2a3a97
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-23 15:35:12 +0330

    perfect-numbers

--- perfect-numbers/perfect_numbers.go
+++ perfect-numbers/perfect_numbers.go
@@ -1,15 +1,23 @@
 package perfect
 
+import "errors"
+
 // Define the Classification type here.
 type Classification byte
 
 const (
-	ClassificationDeficient Classification = iota // aliquot sum < number
+	ClassificationUndefined Classification = iota // undefined
+	ClassificationDeficient                       // aliquot sum < number
 	ClassificationPerfect                         // aliquot sum = number
 	ClassificationAbundant                        // aliquot sum > number
 )
 
+var ErrOnlyPositive = errors.New("only positive numbers")
+
 func Classify(number int64) (Classification, error) {
+	if number <= 0 {
+		return ClassificationUndefined, ErrOnlyPositive
+	}
 	sum := int64(0)
 	for i := int64(1); i < number; i++ {
 		if number%i == 0 {
