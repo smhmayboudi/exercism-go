f01d8c4eaf393c0fcd95520b1939fce5511feee5
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-23 16:27:09 +0330

    pig-latin

--- pig-latin/pig_latin.go
+++ pig-latin/pig_latin.go
@@ -1,41 +1,32 @@
 package piglatin
 
-import (
-	"strings"
-)
+import "strings"
 
-// Sentence doc here
-func Sentence(sentence string) string {
-	converter := func(word string) string {
-		qu := strings.Index(word, "qu")
-		y := strings.Index(word, "y")
-		vowel := strings.IndexAny(word, "aeiou")
-		if len(word) >= 2 && (word[:2] == "xr" || word[:2] == "yt") {
-			vowel = 0
+// isVowel returns true for a,e,i,o,u.
+func isVowel(c byte) bool { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' }
+
+// findVowel finds the first vowel sound in the word.
+func findVowel(word string) int {
+	for i, c := range []byte(word) {
+		if isVowel(c) && (i == 0 || word[i-1] != 'q' || c != 'u') {
+			return i
+		} else if (c == 'y' || c == 'x') && (i == len(word)-1 || !isVowel(word[i+1])) {
+			return i
 		}
-
-		index := 0
-
-		if vowel == 0 {
-			index = 0
-		} else if y == 1 && len(word) == 2 {
-			word = strings.Join([]string{word[1:], word[:1]}, "")
-			index = 0
-		} else if y > 0 && (y < vowel || vowel == -1) {
-			index = y
-		} else if qu >= 0 {
-			index = qu + 2
-		} else {
-			index = vowel
-		}
-		return strings.Join([]string{word[index:], word[:index], "ay"}, "")
 	}
+	return len(word)
+}
 
-	words := strings.Split(sentence, " ")
-	converted := make([]string, len(words))
+// Sentence converts message to pig latin
+func Sentence(input string) string {
+
+	// For each word in input
+	words := strings.Fields(input)
 	for i, word := range words {
-		converted[i] = converter(word)
-	}
 
-	return strings.Join(converted, " ")
+		// Find the first j sound, and modify word
+		j := findVowel(word)
+		words[i] = word[j:] + word[:j] + "ay"
+	}
+	return strings.Join(words, " ")
 }
