48632cd59f673f4e8c133f3b8e785e7737592d0b
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-23 07:36:27 +0330

    matrix

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -3,114 +3,66 @@ package matrix
 import (
 	"errors"
 	"strconv"
+	"strings"
 )
 
-// Define the Matrix type here.
 type Matrix [][]int
 
-func New(s string) (Matrix, error) {
-	runes := []rune(s)
-	le := len(runes)
+func New(inputString string) (Matrix, error) {
+	rows := strings.Split(inputString, "\n")
+	result := make(Matrix, len(rows))
 
-	if runes[le-1] == '\n' {
-		return nil, errors.New("error \\n at end")
-	}
+	for index, line := range rows {
+		columns := strings.Split(strings.TrimSpace(line), " ")
+		result[index] = make([]int, len(columns))
 
-	numMR := [][]int{}
-	numMC := []int{}
-	for i := 0; i < le; {
-		numS := ""
-
-		for j := i; j < le; j++ {
-			numC := runes[j]
-			if '0' <= numC && numC <= '9' {
-				numS = numS + string(numC)
-				if j+1 == le {
-					if numS != "" {
-						num, err := strconv.Atoi(numS)
-						if err != nil {
-							return nil, errors.New("error strconv")
-						}
-						numMC = append(numMC, num)
-					}
-
-					numMR = append(numMR, numMC)
-					numMC = []int{}
-					i = i + len(numS) + 1
-					break
-				}
-			} else if numC == '\n' {
-				if numS != "" {
-					num, err := strconv.Atoi(numS)
-					if err != nil {
-						return nil, errors.New("error strconv")
-					}
-					numMC = append(numMC, num)
-				}
-
-				numMR = append(numMR, numMC)
-				numMC = []int{}
-				i = i + len(numS) + 1
-				break
-			} else if numC == ' ' {
-				if numS != "" {
-					num, err := strconv.Atoi(numS)
-					if err != nil {
-						return nil, errors.New("error strconv")
-					}
-					numMC = append(numMC, num)
-				}
-
-				i = i + len(numS) + 1
-				break
-			} else {
-				return nil, errors.New("error")
+		for position, element := range columns {
+			num, err := strconv.Atoi(element)
+			if err != nil {
+				return nil, errors.New("Problem converting number")
 			}
+			result[index][position] = num
 		}
 	}
-
-	invalid := false
-	invalid = invalid || len(numMR) == 1 && len(numMR[0]) == 1 && numMR[0][0] != 0
+	return result, result.validate()
+}
+func (result Matrix) validate() error {
 	for r := 0; r < len(numMR)-1; r++ {
-		invalid = invalid || (len(numMR[r]) != len(numMR[r+1]))
-	}
-	if invalid {
-		return nil, errors.New("error")
-	}
-
-	return numMR, nil
-}
-
-// Cols and Rows must return the results without affecting the matrix.
-func (m Matrix) Cols() [][]int {
-	mN := Matrix{}
-	// for r := 0; r < len(m); r++ {
-	for c := 0; c < len(m[0]); c++ {
-		col := []int{}
-		for r := 0; r < len(m); r++ {
-			col = append(col, m[r][c])
+		if len(numMR[r]) != len(numMR[r+1]) {
+			return errors.New("Invalid matrix")
 		}
-		mN = append(mN, col)
 	}
-	return mN
+	return nil
 }
 
-func (m Matrix) Rows() [][]int {
-	mN := Matrix{}
-	for r := 0; r < len(m); r++ {
-		row := []int{}
-		for c := 0; c < len(m[r]); c++ {
-			row = append(row, m[r][c])
+func (matrix Matrix) Rows() [][]int {
+	rows := make([][]int, len(matrix))
+
+	for index, row := range matrix {
+		rows[index] = make([]int, len(row))
+
+		for position, element := range row {
+			rows[index][position] = element
 		}
-		mN = append(mN, row)
 	}
-	return mN
+	return rows
 }
 
-func (m Matrix) Set(row, col, val int) bool {
-	if row < 0 || len(m) <= row || col < 0 || len(m[row]) <= col {
-		return false
+func (matrix Matrix) Cols() [][]int {
+	cols := make([][]int, len(matrix[0]))
+
+	for position, _ := range cols {
+		cols[position] = make([]int, len(matrix))
+
+		for index, _ := range matrix {
+			cols[position][index] = matrix[index][position]
+		}
 	}
-	m[row][col] = val
-	return true
+	return cols
+}
+func (matrix Matrix) Set(row, col, val int) (ok bool) {
+	if ok = row >= 0 && col >= 0 && row < len(matrix) && col < len(matrix[0]); ok {
+		matrix[row][col] = val
+	}
+	return
 }
