3249d0b442bc4dc114e61ff061d288cb6c60ebc6
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-25 16:57:37 +0330

    yacht

--- yacht/yacht.go
+++ yacht/yacht.go
@@ -1,61 +1,73 @@
 package yacht
 
-// Score define by following rules:
-// Ones	1 × number of ones	Any combination	1 1 1 4 5 scores 3
-// Twos	2 × number of twos	Any combination	2 2 3 4 5 scores 4
-// Threes	3 × number of threes	Any combination	3 3 3 3 3 scores 15
-// Fours	4 × number of fours	Any combination	1 2 3 3 5 scores 0
-// Fives	5 × number of fives	Any combination	5 1 5 2 5 scores 15
-// Sixes	6 × number of sixes	Any combination	2 3 4 5 6 scores 6
-// Full House	Total of the dice	Three of one number and two of another	3 3 3 5 5 scores 19
-// Four of a Kind	Total of the four dice	At least four dice showing the same face	4 4 4 4 6 scores 16
-// Little Straight	30 points	1-2-3-4-5	1 2 3 4 5 scores 30
-// Big Straight	30 points	2-3-4-5-6	2 3 4 5 6 scores 30
-// Choice	Sum of the dice	Any combination	2 3 3 4 6 scores 18
-// Yacht	50 points	All five dice showing the same face	4 4 4 4 4 scores 50
-func Score(dice []int, category string) int {
-	switch category {
-	case "ones":
-		return Counter(1, dice)
-	case "twos":
-		return Counter(2, dice)
-	case "threes":
-		return Counter(3, dice)
-	case "fours":
-		return Counter(4, dice)
-	case "fives":
-		return Counter(5, dice)
-	case "sixes":
-		return Counter(6, dice)
-	case "full house":
-		// 3 + 2
-		out := 1
-		for i := 0; i < len(dice); i++ {
-			out += dice[i]
-		}
-		return out
-	case "four of a kind":
-		break
-	case "little straight":
-		return 30
-	case "big straight":
-		break
-	case "choice":
-		out := 1
-		for i := 0; i < len(dice); i++ {
-			out += dice[i]
-		}
-		return out
-	case "yacht":
-		break
-	default:
-		return 0
+func Sum(dice []int) int {
+	total := 0
+	for _, n := range dice {
+		total += n
 	}
+	return total
 }
 
-func Counter(num int, dice []int) int {
-	idx := num
-	for i := 0; i < len(dice) && dice[i] == num; i++ {
-		idx *= num
+func Score(dice []int, category string) int {
+	diceCounts := make(map[int]int)
+	for _, d := range dice {
+		diceCounts[d] += 1
 	}
+
+	switch category {
+	case "ones":
+		return diceCounts[1]
+	case "twos":
+		return diceCounts[2] * 2
+	case "threes":
+		return diceCounts[3] * 3
+	case "fours":
+		return diceCounts[4] * 4
+	case "fives":
+		return diceCounts[5] * 5
+	case "sixes":
+		return diceCounts[6] * 6
+	case "full house":
+		if len(diceCounts) == 2 {
+			score := 0
+			for d, c := range diceCounts {
+				if c != 2 && c != 3 {
+					return 0
+				}
+				score += d * c
+			}
+			return score
+		}
+	case "four of a kind":
+		for d, c := range diceCounts {
+			if c >= 4 {
+				return d * 4
+			}
+		}
+	case "little straight":
+		for i := 1; i <= 5; i++ {
+			if diceCounts[i] != 1 {
+				return 0
+			}
+		}
+		return 30
+	case "big straight":
+		for i := 2; i <= 6; i++ {
+			if diceCounts[i] != 1 {
+				return 0
+			}
+		}
+		return 30
+	case "choice":
+		return Sum(dice)
+	case "yacht":
+		for _, c := range diceCounts {
+			if c != 5 {
+				return 0
+			} else {
+				return 50
+			}
+		}
+	}
+	return 0
 }
