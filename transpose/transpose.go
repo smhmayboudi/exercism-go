a458f4b3086a9344c9709eac8c56d6c371248b91
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-26 14:58:20 +0330

    transpose

--- transpose/transpose.go
+++ transpose/transpose.go
@@ -1,21 +1,31 @@
 package transpose
 
-func Transpose(input []string) []string {
-	rows := len(input)
-	if rows == 0 {
-		return []string{}
-	}
-	cols := len(input[0])
-	for _, v := range input {
-		if cols < len(v) {
-			cols = len(v)
+// Transpose returns a transposed matrix of characters
+func Transpose(given []string) []string {
+	// first, determine the size of the output as the
+	// length of the longest input string
+	outLen := 0
+	for i := 0; i < len(given); i++ {
+		if n := len(given[i]); n > outLen {
+			outLen = n
 		}
 	}
-	out := make([]string, cols)
-	for _, value := range input {
-		for col, val := range value {
-			out[col] += string(val)
+
+	maxOut := len(given) // maxOut is the length of the longest string in the output
+	out := make([]string, outLen)
+	s := make([]byte, maxOut) // s will hold output strings as we construct them
+
+	for i := 0; i < outLen; i++ {
+		last := 0
+		for j := 0; j < maxOut; j++ {
+			if g := given[j]; len(g) > i {
+				s[j] = g[i]
+				last = j
+			} else {
+				s[j] = ' '
+			}
 		}
+		out[i] = string(s[:last+1])
 	}
 	return out
 }
