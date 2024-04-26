c6b4b3c52b5cb423af15cd613bf211fe17a1d69d
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-26 08:48:20 +0330

    binary-search

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -3,16 +3,16 @@ package binarysearch
 // SearchInts performs binary search returning index of the match.
 // -1 if the key is not found or array is 0 length.
 func SearchInts(list []int, key int) int {
-	l := 0
-	r := len(list) - 1
-	for l <= r {
-		c := (l + r) / 2
-		if list[c] == key {
-			return c
-		} else if list[c] < key {
-			l = c + 1
-		} else {
-			r = c - 1
+	for left, right := 0, len(list); left != right; {
+		mid := (left + right) / 2
+
+		switch {
+		case list[mid] == key:
+			return mid
+		case key < list[mid]:
+			right = mid
+		default:
+			left = mid + 1
 		}
 	}
 	return -1
