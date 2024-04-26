912b785bad7448617d972718e725e20647cf5fab
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-26 19:32:21 +0330

    matching-brackets

--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,11 +1,23 @@
 package brackets
 
+var match = map[rune]rune{
+	')': '(',
+	'}': '{',
+	']': '[',
+}
+
 func Bracket(input string) bool {
-	out := map[rune]int{}
-	for _, value := range input {
-		if value == '{' || value == '}' || value == '[' || value == ']' || value == '(' || value == ')' {
-			out[value]++
+	brackets := []rune{}
+	for _, r := range input {
+		switch r {
+		case '(', '{', '[':
+			brackets = append(brackets, r)
+		case ')', '}', ']':
+			if len(brackets) == 0 || brackets[len(brackets)-1] != match[r] {
+				return false
+			}
+			brackets = brackets[:len(brackets)-1]
 		}
 	}
-	return out['{'] == out['}'] && out['['] == out[']'] && out['('] == out[')']
+	return len(brackets) == 0
 }
