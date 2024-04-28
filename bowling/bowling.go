cd11900f96ab4855c6cd48f91e43218b32135165
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-04-28 10:56:53 +0330

    bowling

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -1,94 +1,103 @@
 package bowling
 
 import (
-	"errors"
+	"fmt"
 )
 
-// Game represents a game of bowling.
+const testVersion = 1
+
 type Game struct {
-	frames        [12]int
-	frameResult   [12]string
-	frameIndex    int
-	throwsInFrame int
-	rolls         [12][2]int
+	round     int
+	prevPins  int
+	data      []int
+	maxRounds int
 }
 
-// NewGame returns a pointer to a new game object.
+// NewGame sets up a new bowling game
 func NewGame() *Game {
-	return &Game{}
+	return &Game{
+		round:     0,
+		prevPins:  -1,
+		data:      make([]int, 0),
+		maxRounds: 10,
+	}
 }
 
-// Roll records a roll for a game of bowling.
+// Roll lets the game know how many pins where knocked down
 func (g *Game) Roll(pins int) error {
-	if pins < 0 {
-		return errors.New("negative pins are not allowed")
+	if g.gameOver() {
+		return fmt.Errorf("Cannot roll after game is over %d", g.round)
 	}
-	if pins > 10 {
-		return errors.New("pins greater than 10 are not allowed")
+
+	if pins > 10 || pins < 0 {
+		return fmt.Errorf("cannot knock down more than 10 pins or less than zero: %d", pins)
 	}
-	if 10-g.frames[g.frameIndex] < pins {
-		return errors.New("cannot score more pins than are remaining")
+
+	if g.prevPins != -1 && pins+g.prevPins > 10 {
+		return fmt.Errorf("cannot knock down more than 10 pins in a round: %d + %d", g.prevPins, pins)
 	}
-	if g.frameIndex == 10 && g.frames[9] != 10 {
-		return errors.New("cannot roll after game is over")
-	}
-	if g.frameIndex == 10 && g.frameResult[9] == "-" && g.throwsInFrame == 1 {
-		return errors.New("cannot roll after the game is over")
-	}
-	if g.frameResult[9] == "X" && g.frameResult[10] == "X" && g.frameResult[11] == "X" {
-		return errors.New("cannot roll after game is over")
-	}
-	if g.frameResult[9] == "X" && g.frameResult[10] == "X" && g.throwsInFrame > 0 {
-		return errors.New("cannot roll after game is over")
-	}
-	if g.frameResult[9] == "X" && g.frameResult[10] != "X" && g.frameIndex > 10 {
-		return errors.New("cannot roll after game is over")
-	}
-	if pins == 10 {
-		g.frameResult[g.frameIndex] = "X"
-	} else if g.frames[g.frameIndex]+pins == 10 {
-		g.frameResult[g.frameIndex] = "-"
-	}
-	g.rolls[g.frameIndex][g.throwsInFrame] = pins
-	g.frames[g.frameIndex] += pins
-	if g.throwsInFrame == 1 || pins == 10 {
-		g.frameIndex++
-		g.throwsInFrame = 0
+	var spare bool
+	if g.prevPins == -1 {
+		g.prevPins = pins
+		if pins == 10 {
+			g.round++
+			g.prevPins = -1
+		} else if g.round >= 10 {
+			g.round++
+		}
+
 	} else {
-		g.throwsInFrame++
+		spare = g.prevPins+pins == 10
+		g.round++
+		g.prevPins = -1
 	}
+	g.data = append(g.data, pins)
+
+	if g.round == 10 {
+		// last frame is a strike
+		if pins == 10 && g.prevPins == -1 {
+			g.maxRounds += 2
+		} else if spare {
+			g.maxRounds++
+		}
+	}
+
 	return nil
 }
 
-// Score returns the total score for a game of bowling.
+func (g *Game) gameOver() bool {
+	return g.round >= g.maxRounds
+}
+
+// Score the finished bowling game
 func (g *Game) Score() (int, error) {
-	if g.frameIndex < 9 {
-		return 0, errors.New("score cannot be taken before the end of the game")
+	if !g.gameOver() {
+		return 0, fmt.Errorf("Game has not finished. Keep Bowling! %d %d", g.round, g.maxRounds)
 	}
-	if g.frameResult[9] == "-" && g.frameIndex < 11 && g.throwsInFrame == 0 {
-		return 0, errors.New("bonus roll for spare must be scored before end of game")
-	}
-	if g.frameResult[9] == "X" && g.frameIndex < 11 {
-		return 0, errors.New("bonus rolls for strike must be scored before end of game")
-	}
-	if g.frameResult[9] == "X" && g.frameResult[10] == "X" && g.throwsInFrame == 0 && g.frameIndex < 12 {
-		return 0, errors.New("bonus rolls for strike must be scored before end of game")
-	}
-	totalScore := 0
-	for index, val := range g.frames {
-		if g.frameResult[index] == "-" && index < 9 {
-			totalScore += g.rolls[index+1][0]
-		}
-		if g.frameResult[index] == "X" && index < 9 {
-			if g.frameResult[index+1] == "X" && g.frameResult[index+2] == "X" {
-				totalScore += 20
-			} else if g.frameResult[index+1] == "X" {
-				totalScore += (10 + g.rolls[index+2][0])
+	score := 0
+	prev := -1
+	round := 0
+	for i, h := range g.data {
+		if prev == -1 {
+			if h == 10 { // strike
+				round++
+				score += h + g.data[i+1] + g.data[i+2]
 			} else {
-				totalScore += g.frames[index+1]
+				prev = h
 			}
+		} else {
+			if h+prev == 10 { //spare
+				score += g.data[i+1]
+			}
+			score += h + prev
+			prev = -1
+			round++
 		}
-		totalScore += val
+		if round == 10 {
+			break
+		}
+
 	}
-	return totalScore, nil
+
+	return score, nil
 }
